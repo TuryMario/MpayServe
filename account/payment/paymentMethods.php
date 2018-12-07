bb697a46b999f00e99611062e31f93916df79b3a
mariotury <mariotury@outlook.com>
2018-12-07 20:04:39 +0300

    New Change to paymentMethods.php

--- account/payment/paymentMethods.php
+++ account/payment/paymentMethods.php
@@ -1,12 +1,40 @@
 <?php
-  $account->subtract_sender_acc_bal($acount->sender_acc,$account->amount){
-    
+  subtract_sender_acc_bal($acount->sender_acc,$account->amount){
+    $account->new_sender_acc_bal = $account->sender_acc_bal - $account->amount;
+
+    //From Account Update
+    UpdateAccount($acount->sender_acc, $account->new_sender_acc_bal );
+
+    //From Account Update
+    UpdateTransactionLog($acount->recepient_telno, $acount->sender_acc, $account->new_sender_acc_bal, $TransactionType,$Status, $TransactionStatus,$TransactionRef, $TransactionStatusCode ,$Created);
   }
 
   //add money to mal
-  $account->add_mal_acc_bal($acount->mal_acc,$account->amount);
+  add_mal_acc_bal($acount->mal_acc,$account->amount){
+    //Get account balance
+    $account->mal_acc_balance = $account->get_acc_balance();
+    //get commission
+
+    $account->mal_new_acc_bal = $account->mal_acc_balance + $account->commission;
+
+    //From Account Update
+    UpdateAccount($acount->mal_acc, $account->mal_new_acc_bal);
+
+    //From TransactionLog Update
+    UpdateTransactionLog($acount->mal_acc, $account->mal_new_acc_bal, $TransactionType,$Status, $TransactionStatus,$TransactionRef, $TransactionStatusCode ,$Created);
+  }
 
   //add money to pixel
-  $account->add_pixel_acc_bal($acount->pixel_acc,$account->amount);
+  add_pixel_acc_bal($acount->pixel_acc,$account->amount){
+    //Get account balance
+    $account->pixel_acc_balance = $account->get_acc_balance();
+    $account->pixel_new_acc_bal = $account->pixel_acc_balance + $account->commission;
+
+    //From Account Update
+    UpdateAccount($acount->pixel_acc, $account->pixel_new_acc_bal);
+
+    //From TransactionLog Update
+    UpdateTransactionLog($acount->pixel_acc, $account->pixel_new_acc_bal, $TransactionType,$Status, $TransactionStatus,$TransactionRef, $TransactionStatusCode ,$Created);
+  }
 
 ?>
